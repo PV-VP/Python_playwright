a35f86a5fd6606f220522ad257490ede61a619f7
PV-VP <pashamazurov@gmail.com>
2026-03-30 20:47:19 +0200

    перевірка переходу на Знайти мебляра перевірка пошуку 17990, переходу на сторінку і перевірка спливаючого вікна

--- Tests/test_locators.py
+++ Tests/test_locators.py
@@ -1,11 +1,18 @@
 from playwright.sync_api import Page, expect
 
-def test_locator_role(page: Page):
+def test_locator_role(page: Page): #тест вияр базар(находим поле, нажимаем, проверяем выпадающий список)
     page.goto('https://viyar.ua/ua/')
-    page.get_by_role("button", name='Меблева фурнітура').click()
-    expect(page).to_have_title('Меблева фурнітура | Viyar.ua | Купити у Києві, Україні')
-    page.get_by_role("button", name='Змінити мову').click()
-    expect(page.get_by_text('Polski')).to_be_visible()
+    page.get_by_role("link", name="Знайти мебляра").click()
+
+
+
+
+def test_locator_placeholder(page: Page):
+    page.goto('https://viyar.ua/ua/')
+    page.get_by_placeholder('Я шукаю...').fill('17990')
+    page.keyboard.press('Enter')
+    expect(page.get_by_text('Інформація про наявність товарів на філії Гавела, 23 може бути неточною. Уточнюйте, будь ласка, наявність у менеджерів чи у контакт-центрі.')).to_be_visible()
+
 
 
     #https://www.youtube.com/watch?v=zSrRTk4IkFY
