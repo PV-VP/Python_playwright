0a2678cc3d43a9be15a93aed015b791d98a39c75
PV-VP <pashamazurov@gmail.com>
2026-04-29 21:46:37 +0200

    Виправлено натискання Казначейство(додали ще підрозділів, не міг найти що тиснути)

--- Odoo/test_create_task.py
+++ Odoo/test_create_task.py
@@ -8,8 +8,7 @@ def test_create_task(page, open_odoo):
     page.get_by_title("Створити Задачу/Заявку").click()                         #клікаєм Створити Задачу/Заявку
     page.get_by_role("button", name="Нова").click()                             #створюєм натиска.чи кнопку Нова
     page.get_by_text("Пошук...").click()                                        #віжкриваєм список підрозділів
-    expect(page.get_by_text("Казначейство")).to_be_visible()                    #очікуєм що буде Казначейство
-    page.locator(".selection_item", has_text="Казначейство").click()            #жмем Казначейство
+    page.get_by_text("Казначейство", exact=True).click()                #жмем Казначейство
     page.get_by_text("Пошук...").blur()                                         #убираєм фокус з пошуку
     page.locator("button.creation_form_tabs_item", has_text="Загальні").click() #Вибір Групи Шаблонів(форма заявки)
     page.get_by_placeholder("Вкажіть назву задачі").fill(generate_task_name())  #заповнюєм назву довільно
