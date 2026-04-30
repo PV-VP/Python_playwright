452d8adc1358efe3f59fc51cba54b34aea9e240d
PV-VP <pashamazurov@gmail.com>
2026-04-30 21:17:42 +0200

    додано заповнення поляВкажіть призначення платежу радомними буквами(20 символів)

--- Odoo/test_create_task_request_for_funding.py
+++ Odoo/test_create_task_request_for_funding.py
@@ -1,6 +1,6 @@
 
 from playwright.sync_api import Page, expect
-from helpers import generate_random_name
+from helpers import generate_random_name, generate_random_fill_20_symbols
 
 def test_create_task(page:Page, open_odoo):
 
@@ -11,9 +11,9 @@ def test_create_task(page:Page, open_odoo):
     page.get_by_text("Казначейство", exact=True).click()                        #жмем Казначейство
     page.get_by_text("Пошук...").blur()                                         #убираєм фокус з пошуку
     page.locator("button.creation_form_tabs_item", has_text="Заявки на кошти").click() #Вибір Групи Шаблонів(форма заявки)
-    page.get_by_text("Заявка на кошти готівкою", exact=True).click()
+    page.get_by_text("Заявка на кошти готівкою", exact=True).click()      #вибераэм готівку
     page.get_by_placeholder("Вкажіть іншу особу, якщо виконавець не є співробітником компанії").fill(generate_random_name())
-
+    page.get_by_placeholder("Вкажіть призначення платежу").fill(generate_random_fill_20_symbols())
 
 
     page.screenshot(path="../screenshot/task.png") #робим скрін
