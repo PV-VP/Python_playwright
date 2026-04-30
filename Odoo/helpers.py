6fff05ee5249a8df2a27bfb4b697f5ab3cd1400b
PV-VP <pashamazurov@gmail.com>
2026-04-30 21:16:00 +0200

    створено фунцію рандомного заповнення поля 20 символами(generate_random_fill_20_symbols())

--- Odoo/helpers.py
+++ Odoo/helpers.py
@@ -4,12 +4,18 @@ import string
 
 def generate_task_name():
     return  "Task " + "".join(random.choices(string.ascii_letters, k=16))  # рандомні данні для назви задачі
+
 def generate_random_name():
     return  "Meeting " + "".join(random.choices(string.ascii_letters, k=6))  # рандомні данні для назви зустрічі
+
 def generate_random_fill():
     return  "".join(random.choices(string.ascii_letters, k=80))  # рандомні данні для опису зустрічі
+
 def generate_random_digit():
     return str((random.randint(1, 21)))
+
+def generate_random_fill_20_symbols():
+    return  "".join(random.choices(string.ascii_letters, k=20))
 
 def generate_fibonacci():
     fib = [1, 1]
